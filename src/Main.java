54789fd599bd29cf4f8a3e80624f03542dad9fb9
lexchs <alexcheremisin@mail.ru>
2024-02-26 11:47:58 +0300

    update main

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ public class Main {
     public static void main(String[] args) {
         PhoneBook phoneBook = new PhoneBook();
 
-        phoneBook.addContact("Иван", "123456789");
-        phoneBook.addContact("Петр", "987654321");
-        phoneBook.addContact("Иван", "987654321");
-        phoneBook.addContact("Мария", "555555555");
-        phoneBook.addContact("Иван", "111111111");
+        phoneBook.addContact("Алексей", "89113456543");
+        phoneBook.addContact("Дмитрий", "89113451834");
+        phoneBook.addContact("Максим", "89113452759");
+        phoneBook.addContact("Максим", "89113453677");
+        phoneBook.addContact("Ирина", "89113454081");
 
         phoneBook.displayPhoneBook();
     }
